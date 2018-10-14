2191e7e62fd3adfc353d310bfddad435ef00ddd3
endlesscadence <22767373+endlesscadence@users.noreply.github.com>
2018-10-14 17:44:31 +0200

    Added switch to build throttle or non-throttle version of the controller. Release script builds both. Release 0.14.1

--- src/controller/compile.bat
+++ src/controller/compile.bat
@@ -1,4 +1,6 @@
 PATH = %PATH%;C:\SDCC\usr\local\bin;%~dp0..\..\tools\cygwin\bin
 
 make -f Makefile_windows clean
-make -f Makefile_windows
+
+:: pass batch file parameters, e.g. THROTTLE=0
+make -f Makefile_windows %*
