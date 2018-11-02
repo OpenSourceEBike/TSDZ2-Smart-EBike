30a42aef13c5ed32dfa38e0e45f848e94c4bb7e4
casainho <casainho@gmail.com>
2018-11-02 12:10:53 +0000

    Improved battery voltage reading on LCD3 and battery SOC graph bars calculation (#36)

--- src/display/KT-LCD3/config.h
+++ src/display/KT-LCD3/config.h
@@ -9,20 +9,19 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
+// This values were taken from a discharge graph of Samsung INR18650-25R cells, at almost no current discharge
+// This graph: https://endless-sphere.com/forums/download/file.php?id=183920&sid=b7fd7180ef87351cabe74a22f1d162d7
+#define LI_ION_CELL_VOLTS_83    3.96
+#define LI_ION_CELL_VOLTS_50    3.70
+#define LI_ION_CELL_VOLTS_17    3.44
+#define LI_ION_CELL_VOLTS_0     3.30
+
+// Battery voltage (readed on motor controller):
+#define ADC_BATTERY_VOLTAGE_PER_ADC_STEP_X10000 866
+
 // Battery voltage (readed on LCD3):
 // 30.0V --> 447 | 0.0671 volts per each ADC unit
 // 40.0V --> 595 | 0.0672 volts per each ADC unit
-
-#define LI_ION_CELL_VOLTS_100   4.06
-#define LI_ION_CELL_VOLTS_80    3.93
-#define LI_ION_CELL_VOLTS_60    3.78
-#define LI_ION_CELL_VOLTS_40    3.60
-#define LI_ION_CELL_VOLTS_20    3.38
-#define LI_ION_CELL_VOLTS_10    3.25
-#define LI_ION_CELL_VOLTS_0     3.00
-
-// Battery voltage (readed on motor controller):
-#define ADC_BATTERY_VOLTAGE_PER_ADC_STEP_X10000 863
 
 // Possible values: 0, 1, 2, 3, 4, 5, 6
 // 0 equal to no filtering and no delay, higher values will increase filtering but will also add bigger delay
